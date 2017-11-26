f4832ae3f64bf0feaed6c247a9fd0d9027b2d14b
damomos <damomos123@gmail.com>
2017-11-26 01:38:45 +0100

    Initial commit

---
+++ app/src/main/java/com/example/princess/webviewapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.princess.webviewapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    String url = "https://firebase.google.com/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setLoadsImagesAutomatically(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(String.valueOf(url));
+
+    }
+}
